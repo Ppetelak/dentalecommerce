e4e67879b8459a74d8155a567d26fd28e9d6d276
Pablo Petelak <p.petelak@gmail.com>
2023-07-20 17:22:28 -0300

    Commit 20/07/2023 ás 17:22

--- js/script.js
+++ js/script.js
@@ -30,4 +30,49 @@ $(document).ready(function () {
             $('#Naoeresponsavel').hide();
         }
     });
+    $('#possuidependentes').change(function() {
+        var selecionado = $(this).val();
+        
+        // Se selecionar "Sim", exibe a seção de dependentes
+        if (selecionado === 'Sim') {
+          $('#existeSim').show();
+        } else {
+          // Caso contrário, esconde a seção de dependentes e remove os dependentes adicionados anteriormente
+          $('#existeSim').hide();
+          $('.dependente').not(':first').remove();
+        }
+      });
+    
+      // Ao clicar em "Adicionar dependente"
+      $('adicionar-dependente').click(function() {
+        var totalDependentes = $('.dependente').length;
+        var novoId = 'dependente-' + (totalDependentes + 1);
+    
+        // Clona a primeira seção de dependente e altera o data-id e os ids dos inputs
+        var clone = $('.dependente:first').clone();
+        clone.attr('data-id', novoId);
+        clone.find('input, select').each(function() {
+          var id = $(this).attr('id');
+          $(this).attr('id', id.replace('dependente-1', novoId));
+        });
+    
+        // Exibe o botão "Excluir dependente" se houver mais de um dependente
+        if (totalDependentes >= 1) {
+          clone.find('excluir-dependente').show();
+        }
+    
+        // Insere o novo dependente após o último dependente existente
+        $('.dependente:last').after(clone);
+      });
+    
+      // Ao clicar em "Excluir dependente"
+      $(document).on('click', 'excluir-dependente', function() {
+        var dependente = $(this).closest('.dependente');
+        var totalDependentes = $('.dependente').length;
+    
+        // Remove o dependente apenas se houver mais de um dependente
+        if (totalDependentes > 1) {
+          dependente.remove();
+        }
+    });
 });
