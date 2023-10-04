51415aa95e6c113611571f6b34d9dcf47b8c8336
Pablo Petelak <p.petelak@gmail.com>
2023-10-04 19:21:10 -0300

    Alterações

--- js/scriptContrato.js
+++ js/scriptContrato.js
@@ -1,31 +1,39 @@
-window.onload = function () {
-    const content = document.querySelector('.content');
-    const header = document.querySelector('.header');
-    const footer = document.querySelector('.footer');
-  
-    // Função para dividir o conteúdo em páginas A4
-    function paginateContent() {
-      const pageHeight = 297; // Altura de uma página A4 em mm
-      let currentPageHeight = 0;
-      let currentPage = null;
-  
-      content.childNodes.forEach((childNode) => {
-        if (!currentPage || currentPageHeight + childNode.clientHeight > pageHeight) {
-          // Criar uma nova página A4
-          currentPage = document.createElement('div');
-          currentPage.classList.add('a4');
-          content.appendChild(currentPage);
-          currentPageHeight = 0;
-  
-          // Clonar cabeçalho e rodapé para a nova página
-          currentPage.appendChild(header.cloneNode(true));
-          currentPage.appendChild(footer.cloneNode(true));
-        }
-  
-        currentPage.appendChild(childNode);
-        currentPageHeight += childNode.clientHeight;
-      });
-    }
-    paginateContent();
-};
+function saveAsPDF() {
+  var element = document.querySelector('main'); // Pode ser substituído por um seletor mais específico
+
+  // Opções para personalizar a conversão PDF
+  var opt = {
+      margin: 0,
+      filename: 'sua_pagina_a4.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { 
+                unit: 'mm', 
+                format: 'a4', 
+                orientation: 'portrait',
+                putOnlyUsedFonts: true,
+                pagesplit: true,
+                callback: function (pdf) {
+                      var numPages = pdf.internal.getNumberOfPages();
+                      
+                      for (var i = 1; i <= numPages; i++) {
+                          pdf.setPage(i);
+                          
+                          // Adiciona cabeçalho
+                          pdf.text('Cabeçalho - Página ' + i, pdf.internal.pageSize.width / 2, 10, 'center');
+                          
+                          // Adiciona rodapé com número de página
+                          pdf.text('Página ' + i + ' de ' + numPages, pdf.internal.pageSize.width / 2, pdf.internal.pageSize.height - 10, 'center');
+                      }
+                  }
+              }
+  };
+
+  // Chame a função html2pdf
+  html2pdf(element, opt);
+}
+
+// Adicione um ouvinte de evento ao botão
+document.getElementById('btnSave').addEventListener('click', saveAsPDF);
+
   
